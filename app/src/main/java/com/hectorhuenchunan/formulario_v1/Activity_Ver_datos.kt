430f5b02529f3b9ef85127d0f3effd1381858093
HectorHuenchunan <hector.huenchunan@gmail.com>
2023-09-28 20:30:42 -0300

    Formulario + Boton para limpiar datos

--- app/src/main/java/com/hectorhuenchunan/formulario_v1/Activity_Ver_datos.kt
+++ app/src/main/java/com/hectorhuenchunan/formulario_v1/Activity_Ver_datos.kt
@@ -3,7 +3,7 @@ package com.hectorhuenchunan.formulario_v1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Ver_datos : AppCompatActivity() {
+class Activity_Ver_datos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ver_datos)
